1b2ed2627157897d323e3581e2ca2dc6c1bf630a
z125470-Eunice-Merce <z125470@shibaura-it.ac.jp>
2025-12-07 14:47:15 +0900

    Update hw9-2.c

--- hw9-2.c
+++ hw9-2.c
@@ -1,1 +1,41 @@
+/*
+Merce Eunice
+z125470
+class : 03/12/2025
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a[3][3];
+    int i,j;
+    double determinant;
+
+    for (i = 1; i <= 3; i++) {
+        for (j = 1; j <= 3; j++) {
+            printf("enter the elements for array [%d][%d] = ", i, j);
+            scanf("%d", &a[i-1][j-1]);
+        }
+    }
+
+
+    printf("You entered \n");
+    for (i = 1; i <= 3; i++) {
+        for (j = 1; j <= 3; j++) {
+            printf("%d ",a[i-1][j-1]);
+        }
+        printf("\n");
+    }
+
+    printf("Output\n");
+    for (i = 1; i <= 3; i++) {
+        for (j = 3; j >= 1; j--) {
+            printf("%d ",a[i-1][j-1]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
 
