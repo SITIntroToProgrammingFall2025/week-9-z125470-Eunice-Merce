9ab48f6a19f67c2a2ca8fdac107dfe60fc272c99
z125470-Eunice-Merce <z125470@shibaura-it.ac.jp>
2025-12-07 20:54:38 +0900

    Update ahw9-2.c

--- ahw9-2.c
+++ ahw9-2.c
@@ -1,1 +1,43 @@
+/*
+Merce Eunice
+z125470
+class : 03/12/2025
+*/
+
+# include <stdio.h>
+# include <math.h>
+
+int main(void)
+{
+    int i, j, temp ;
+
+    int a[5] ;
+
+    for (i = 0 ; i < 5 ; i++)
+    {
+        scanf("%d", &a[i]) ;
+    }
+
+    for (i = 0 ; i < 4 ; i++)
+    {
+        for (j = 0 ; j < 4 ; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                temp = a[j] ;
+                a[j] = a[j + 1] ;
+                a[j + 1] = temp ;
+            }
+        }
+    }
+
+    printf("sorted array:\n") ;
+    for (i = 0 ; i < 5 ; i++)
+    {
+        printf("%d ", a[i]) ;
+    }
+
+    return 0 ;
+}
+
 
